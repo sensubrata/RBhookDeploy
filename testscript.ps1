fa4426c1774b93c543bc4bd690994157fc181383
Subrata Sen <subrata.sen@finomial.com>
2015-11-06 03:09:21 +0530

    correction

--- testscript.ps1
+++ testscript.ps1
@@ -1,4 +1,4 @@
 
-$value = (ls env:testvar).value
+[string]$value = (ls env:testvar).value
 $value
-(Get-Content index.html) | ForEach-Object { $_ -replace "test", %testvar% } | Set-Content index.html
+(Get-Content index.html) | ForEach-Object { $_ -replace "test", $value } | Set-Content index.html
