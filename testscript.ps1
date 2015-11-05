ddd3e2907120fc683a9021f9b8a4b8affe39f427
Subrata Sen <subrata.sen@finomial.com>
2015-11-06 03:19:13 +0530

    corrections

--- testscript.ps1
+++ testscript.ps1
@@ -1,4 +1,4 @@
 
 [string]$value = (ls env:testvar).value
 $value
-(Get-Content index.html) | ForEach-Object { $_ -replace "test", $value } | Set-Content index.html
+(Get-Content "..\wwwroot\index.html") | ForEach-Object { $_ -replace "test", $value } | Set-Content "..\wwwroot\index.html"
